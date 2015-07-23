b9717f8f0e1b912ab9db4599e93f268f9c4355e1
Noah <noah@noahveltman.com>
2015-07-23 12:04:42 -0400

    Switching this up

--- index.js
+++ index.js
@@ -1,52 +1,80 @@
-'use strict';
+"use strict";
 
-exports.extract = function (maybeUrl) {
-	return maybeUrl.split('?')[1] || '';
-};
+function addParams(url,params) {
 
-exports.parse = function (str) {
-	if (typeof str !== 'string') {
-		return {};
-	}
+  var qi = url.indexOf("?"),
+      stringified,
+      base = url,
+      search = {};
 
-	str = str.trim().replace(/^(\?|#|&)/, '');
+  if (qi >= 0) {
+    base = url.slice(0,qi);
+    search = parseQueryString(url.slice(qi));
+  }
 
-	if (!str) {
-		return {};
-	}
+  if (params) {
+    for (var key in params) {
+      search[key] = params[key];
+    }
+  }
 
-	return str.split('&').reduce(function (ret, param) {
-		var parts = param.replace(/\+/g, ' ').split('=');
-		var key = parts[0];
-		var val = parts[1];
+  stringified = stringifyQueryString(search);
 
-		key = decodeURIComponent(key);
-		// missing `=` should be `null`:
-		// http://w3.org/TR/2012/WD-url-20120524/#collect-url-parameters
-		val = val === undefined ? null : decodeURIComponent(val);
+  return base + (stringified ? "?" + stringified : "");
 
-		if (!ret.hasOwnProperty(key)) {
-			ret[key] = val;
-		} else if (Array.isArray(ret[key])) {
-			ret[key].push(val);
-		} else {
-			ret[key] = [ret[key], val];
-		}
+  function stringifyQueryString(params) {
+    var pairs = [];
+    for (var key in params) {
 
-		return ret;
-	}, {});
-};
+      var val = params[key];
 
-exports.stringify = function (obj) {
-	return obj ? Object.keys(obj).sort().map(function (key) {
-		var val = obj[key];
+      if (val === true) {
+        val = "true";
+      } else if (val === false) {
+        val = "false";
+      }
 
-		if (Array.isArray(val)) {
-			return val.sort().map(function (val2) {
-				return encodeURIComponent(key) + '=' + encodeURIComponent(val2);
-			}).join('&');
-		}
+      pairs.push(encodeURIComponent(key) + "=" + encodeURIComponent(val));
 
-		return encodeURIComponent(key) + '=' + encodeURIComponent(val);
-	}).join('&') : '';
-};
+    }
+
+    return pairs.join("&");
+  }
+
+  // From https://github.com/sindresorhus/query-string
+  function parseQueryString(str) {
+    if (typeof str !== "string") {
+      return {};
+    }
+
+    str = str.trim().replace(/^(\?|#|&)/, "");
+
+    if (!str) {
+      return {};
+    }
+
+    return str.split("&").reduce(function (ret, param) {
+      var parts = param.replace(/\+/g, " ").split("=");
+      var key = parts[0];
+      var val = parts[1];
+
+      key = decodeURIComponent(key);
+      // missing `=` should be `null`:
+      // http://w3.org/TR/2012/WD-url-20120524/#collect-url-parameters
+      val = val === undefined ? null : decodeURIComponent(val);
+
+      if (!ret.hasOwnProperty(key)) {
+        ret[key] = val;
+      } else if (Array.isArray(ret[key])) {
+        ret[key].push(val);
+      } else {
+        ret[key] = [ret[key], val];
+      }
+
+      return ret;
+    }, {});
+  }
+
+}
+
+module.exports = addParams;
